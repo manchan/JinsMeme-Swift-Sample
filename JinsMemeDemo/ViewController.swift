e5bb41f681e90cc4f8a24c9bfe5d333d365f583b
yuichi matsuoka <anything12new14@gmail.com>
2015-11-21 00:19:34 +0900

    Initial Commit

---
+++ JinsMemeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JinsMemeDemo
+//
+//  Created by matz on 2015/11/21.
+//  Copyright © 2015年 matz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
